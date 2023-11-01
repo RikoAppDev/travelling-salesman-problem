8c84503963c7a5fb958dce577ee4a8baa42da514
RikoAppDev <riko.appdev@gmail.com>
2023-11-01 15:01:05 +0100

    world - nearest neighbour algo, distance matrix

--- World.py
+++ World.py
@@ -4,10 +4,14 @@ from constants import *
 
 
 class World:
-    places = []
+    places: list
+    distance_matrix: list[list]
+    nearest_neighbour_permutation: list
 
     def __init__(self, amount):
         self.places = self.generate_places(int(amount))
+        self.distance_matrix = self.fill_distance_matrix()
+        self.nearest_neighbour_permutation = self.get_nearest_neighbour()
 
     @staticmethod
     def check_place_collision(x, y, actual_places: list):
@@ -34,11 +38,25 @@ class World:
                 count += 1
                 continue
 
+            w_place.setdefault("id", "uid" + str(total - count - 1))
             w_place.setdefault("lat", x)
             w_place.setdefault("long", y)
             w_places.append(w_place)
 
+        print(f"\rGenerating world: loading 100% ✅", end="")
         return w_places
+
+    def fill_distance_matrix(self):
+        distance_matrix = []
+
+        for p1 in self.places:
+            distances = []
+            for p2 in self.places:
+                distances.append(self.calculate_distance(p1, p2))
+
+            distance_matrix.append(distances)
+
+        return distance_matrix
 
     @staticmethod
     def calculate_distance(place1, place2):
@@ -57,3 +75,29 @@ class World:
         value += self.calculate_distance(permutation[amount - 1], permutation[0])
 
         return value
+
+    def get_nearest_neighbour(self):
+        start_place = random.randint(0, len(self.places) - 1)
+        permutation = [self.get_place_with_uid(start_place)]
+
+        visited = []
+        actual_place = start_place
+        while len(permutation) != len(self.places):
+            minimum = max(self.distance_matrix[actual_place])
+
+            index = 0
+            for distance in self.distance_matrix[actual_place]:
+                if index != actual_place and self.get_place_with_uid(index) not in permutation and distance < minimum:
+                    actual_place = index
+                    minimum = distance
+                index += 1
+
+            visited.append(actual_place)
+            permutation.append(self.get_place_with_uid(actual_place))
+
+        return permutation
+
+    def get_place_with_uid(self, n):
+        for p in self.places:
+            if p.get("id") == "uid" + str(n):
+                return p
