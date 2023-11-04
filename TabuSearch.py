254cc76ff282863493f8fc7af1a3d335d2b7cfa6
RikoAppDev <riko.appdev@gmail.com>
2023-11-04 14:08:44 +0100

    tabu search - 2opt neighbourhood generation

--- TabuSearch.py
+++ TabuSearch.py
@@ -29,7 +29,7 @@ class TabuSearch:
 
         i = 0
         while not i == self.max_iterations:
-            neighborhood = self.generate_neighborhood(best_candidate)
+            neighborhood = self.get_neighborhood(best_candidate)
             best_candidate = neighborhood[0]
             for candidate in neighborhood:
                 if (not tabu_list.__contains__(candidate)) and (self.fitness(candidate) > self.fitness(best_candidate)):
@@ -56,6 +56,18 @@ class TabuSearch:
     def fitness(self, permutation):
         return self.nearest_neighbour_value / self.world.get_permutation_value(permutation)
 
+    def get_neighborhood(self, permutation):
+        neighbor = permutation[:]
+        neighborhood = []
+
+        function = random.choice([0, 1])
+        if function == 0:
+            neighborhood = self.generate_neighborhood(neighbor)
+        elif function == 1:
+            neighborhood = self.two_opt_neighborhood(neighbor)
+
+        return neighborhood
+
     @staticmethod
     def generate_neighborhood(permutation):
         neighborhood = []
@@ -63,6 +75,18 @@ class TabuSearch:
             for j in range(i + 1, len(permutation)):
                 neighbor = permutation[:]
                 neighbor[i], neighbor[j] = neighbor[j], neighbor[i]
+                neighborhood.append(neighbor)
+
+        return neighborhood
+
+    @staticmethod
+    def two_opt_neighborhood(permutation):
+        amount = len(permutation)
+        neighborhood = []
+
+        for i in range(amount - 1):
+            for j in range(i + 1, amount):
+                neighbor = permutation[:i] + list(reversed(permutation[i:j])) + permutation[j:]
                 neighborhood.append(neighbor)
 
         return neighborhood
